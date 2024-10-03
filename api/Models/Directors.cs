9d0a54da5607590e5014063ddd7162923ef41ed9
abbas-a-k <a2cuber20205020@gmail.com>
2024-10-03 20:02:40 +0330

    Create Directors model

---
+++ api/Models/Directors.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Directors
+    {
+        public int Id { get; set; }
+        public string ImageUrl { set; get; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Alive { get; set; } = true;
+        public string BirthPlace { get; set; } = string.Empty;
+        public string About { get; set;} = string.Empty;
+        // Not stored in the database
+        //public List<Movies> MyProperty { get; set; }
+    }
+}
